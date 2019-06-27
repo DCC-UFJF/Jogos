b96d6172b8450dc9ee94e6a922f00cf54dbd2b9b
Denner Efisio <dennerefisio@gmail.com>
2019-06-27 16:43:22 -0300

    adicionando classe Tetramino e elementos de Board e Tetris

--- main.cpp
+++ main.cpp
@@ -35,6 +35,14 @@ typedef struct Tetramino
     //Falta definir rotações
 };
 
+typedef struct Board
+{
+    int largura;
+    int altura;
+    int borda;
+    //Falta definir canvas,grid,the board e status_msg
+};
+
 Retangulo criaRetangulo(Ponto p1, Ponto p2)
 {
     Retangulo r;
@@ -42,7 +50,6 @@ Retangulo criaRetangulo(Ponto p1, Ponto p2)
     r.p2 = p2;
     return r;
 }
-
 
 Bloco criaBloco(Ponto p)
 {
@@ -62,7 +69,8 @@ Bloco criaBloco(Ponto p)
     return b;
 }
 
-void criaTetraI(Ponto coords[4], Ponto centro){
+void criaTetraI(Ponto coords[4], Ponto centro)
+{
     coords[0].x = centro.x-2;
     coords[0].y = centro.y;
     coords[1].x = centro.x-1;
@@ -73,7 +81,8 @@ void criaTetraI(Ponto coords[4], Ponto centro){
     coords[3].y = centro.y;
 }
 
-void criaTetraJ(Ponto coords[4], Ponto centro){
+void criaTetraJ(Ponto coords[4], Ponto centro)
+{
     coords[0].x = centro.x-1;
     coords[0].y = centro.y;
     coords[1].x = centro.x;
@@ -84,7 +93,8 @@ void criaTetraJ(Ponto coords[4], Ponto centro){
     coords[3].y = centro.y+1;
 }
 
-void criaTetraL(Ponto coords[4], Ponto centro){
+void criaTetraL(Ponto coords[4], Ponto centro)
+{
     coords[0].x = centro.x-1;
     coords[0].y = centro.y;
     coords[1].x = centro.x;
@@ -95,7 +105,8 @@ void criaTetraL(Ponto coords[4], Ponto centro){
     coords[3].y = centro.y+1;
 }
 
-void criaTetraO(Ponto coords[4], Ponto centro){
+void criaTetraO(Ponto coords[4], Ponto centro)
+{
     coords[0].x = centro.x;
     coords[0].y = centro.y;
     coords[1].x = centro.x-1;
@@ -106,7 +117,8 @@ void criaTetraO(Ponto coords[4], Ponto centro){
     coords[3].y = centro.y+1;
 }
 
-void criaTetraS(Ponto coords[4], Ponto centro){
+void criaTetraS(Ponto coords[4], Ponto centro)
+{
     coords[0].x = centro.x;
     coords[0].y = centro.y;
     coords[1].x = centro.x;
@@ -117,7 +129,8 @@ void criaTetraS(Ponto coords[4], Ponto centro){
     coords[3].y = centro.y+1;
 }
 
-void criaTetraT(Ponto coords[4], Ponto centro){
+void criaTetraT(Ponto coords[4], Ponto centro)
+{
     coords[0].x = centro.x-1;
     coords[0].y = centro.y;
     coords[1].x = centro.x;
@@ -128,7 +141,8 @@ void criaTetraT(Ponto coords[4], Ponto centro){
     coords[3].y = centro.y+1;
 }
 
-void criaTetraZ(Ponto coords[4], Ponto centro){
+void criaTetraZ(Ponto coords[4], Ponto centro)
+{
     coords[0].x = centro.x-1;
     coords[0].y = centro.y;
     coords[1].x = centro.x;
@@ -147,61 +161,139 @@ Tetramino criaTetramino(Ponto centro, int tipo)
 
     switch(tipo)
     {
-        case 0:
-            criaTetraI(coords,centro);
-            t.shift_rotacao = 1;
-            t.centro.x = coords[2].x;
-            t.centro.y = coords[2].y;
-            break;
-        case 1:
-            criaTetraJ(coords,centro);
-            t.shift_rotacao = 0;
-            t.centro.x = coords[1].x;
-            t.centro.y = coords[1].y;
-            break;
-        case 2:
-            criaTetraL(coords,centro);
-            t.centro.x = coords[1].x;
-            t.centro.y = coords[1].y;
-            t.shift_rotacao = 0;
-            break;
-        case 3:
-            criaTetraO(coords,centro);
-            t.centro.x = coords[0].x;
-            t.centro.y = coords[0].y;
-            //falta definir o override
-            break;
-        case 4:
-            criaTetraS(coords,centro);
-            t.shift_rotacao = 1;
-            t.dir_rotacao = -1;
-            t.centro.x = coords[0].x;
-            t.centro.y = coords[0].y;
-            break;
-        case 5:
-            criaTetraT(coords,centro);
-            t.shift_rotacao = 0;
-            t.centro.x = coords[1].x;
-            t.centro.y = coords[1].y;
-            break;
-        case 6:
-            criaTetraZ(coords,centro);
-            t.shift_rotacao = 1;
-            t.dir_rotacao = -1;
-            t.centro.x = coords[1].x;
-            t.centro.y = coords[1].y;
+    case 0:
+        criaTetraI(coords,centro);
+        t.shift_rotacao = 1;
+        t.centro.x = coords[2].x;
+        t.centro.y = coords[2].y;
+        break;
+    case 1:
+        criaTetraJ(coords,centro);
+        t.shift_rotacao = 0;
+        t.centro.x = coords[1].x;
+        t.centro.y = coords[1].y;
+        break;
+    case 2:
+        criaTetraL(coords,centro);
+        t.centro.x = coords[1].x;
+        t.centro.y = coords[1].y;
+        t.shift_rotacao = 0;
+        break;
+    case 3:
+        criaTetraO(coords,centro);
+        t.centro.x = coords[0].x;
+        t.centro.y = coords[0].y;
+        //falta definir o override
+        break;
+    case 4:
+        criaTetraS(coords,centro);
+        t.shift_rotacao = 1;
+        t.dir_rotacao = -1;
+        t.centro.x = coords[0].x;
+        t.centro.y = coords[0].y;
+        break;
+    case 5:
+        criaTetraT(coords,centro);
+        t.shift_rotacao = 0;
+        t.centro.x = coords[1].x;
+        t.centro.y = coords[1].y;
+        break;
+    case 6:
+        criaTetraZ(coords,centro);
+        t.shift_rotacao = 1;
+        t.dir_rotacao = -1;
+        t.centro.x = coords[1].x;
+        t.centro.y = coords[1].y;
     }
 
-    for(int i=0; i<4;i++)
-        {
-            t.blocos[i] = criaBloco(coords[i]);
-        }
+    for(int i=0; i<4; i++)
+    {
+        t.blocos[i] = criaBloco(coords[i]);
+    }
 
     return t;
 }
 
+Board criaBoard(int largura, int altura, int borda)
+{
+    Board b;
+    b.largura = largura;
+    b.altura = altura;
+    b.borda = borda;
+
+//falta definir outras variáveis e desenhar coisas.
+    return b;
+}
+
+int podeMover(Board b, int x, int y)
+{
+    if((x < b.borda) || (x >=b.largura+b.borda) || (y >= b.altura))
+    {
+        return 0;
+    }
+    //else if(x,y in self.grid) return 0; Implementar essa linha
+    else
+        return 1;
+}
+
+void adicionaForma(Board b, Tetramino t)
+{
+    Bloco blocos[4];
+    for(int i=0; i<4; i++)
+    {
+        blocos[i] = t.blocos[i];
+    }
+    //for(int i=0; i<4; i++)
+    //{
+        //b.grid =(blocos[i].x, blocos[i].y)
+    //}
+
+}
+
+void deletaLinha(Board b, int y)
+{
+
+
+}
+
+int verificarLinhaCompleta(Board b, int y)
+{
+
+
+}
+
+void moveLinhaInferior(Board b, int yInicial)
+{
+
+
+}
+
+void removeLinhaCompleta(Board b)
+{
+
+
+}
+
+void gameOver(Board b)
+{
+printf("Game Over");
+}
+
+void pause(Board b)
+{
+
+}
+
+
+
 int main()
 {
+    int larguraBoard = 10;
+    int alturaBoard = 20;
+    int bordaBoard = 2;
+    int delayInicial = 1000;
+
+
 
     // Create the main window
     RenderWindow window(sf::VideoMode(400, 600), "Tetris!");
